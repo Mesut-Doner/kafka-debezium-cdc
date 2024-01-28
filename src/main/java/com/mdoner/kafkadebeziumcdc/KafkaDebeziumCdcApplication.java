71d9825a6bf6920ac0bb54af43e1984aea5135e4
mesut-dnr <mesutdonerng@gmail.com>
2024-01-28 18:06:39 +0300

    init

---
+++ src/main/java/com/mdoner/kafkadebeziumcdc/KafkaDebeziumCdcApplication.java
@@ -0,0 +1,13 @@
+package com.mdoner.kafkadebeziumcdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDebeziumCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDebeziumCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdoner/kafkadebeziumcdc/KafkaDebeziumCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdoner.kafkadebeziumcdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDebeziumCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
